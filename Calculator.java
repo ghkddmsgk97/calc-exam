d277696ab701a8d378e821a6c76f779c9949adf9
EunhaHwang <dmsgk9719@gmail.com>
2021-07-24 14:19:24 +0900

    add feature to Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,13 @@
 package calc;
 
 public class Calculator {
-    // 빼기
-    public int minus (int a, int b) {
+    // 더하기
+    public int add(int a, int b) {
         return a + b;
+    }
+
+    // 빼기
+    public int minus(int a, int b) {
+        return a - b;
     }
 }
